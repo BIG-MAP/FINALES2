bdc8fe5ca3a860057f8e3102fff875ba7d8b3749
MonikaVo <monika.vogler@kit.edu>
2023-03-11 13:06:47 +0100

    Information about requirements moved - requirements now included in the setup.cfg file instead of setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="FINALES2",
-    version="0.0.1",
-    install_requires=[
-        "fastapi",
-        "jose",
-        "passlib",
-        "pytest",
-        "python_jose",
-        "requests",
-        "setuptools",
-        "strawberry",
-        "uvicorn",
-    ],
-    py_modules=[],
-)
+setup()
