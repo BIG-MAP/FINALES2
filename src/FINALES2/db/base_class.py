c40ef65a8d9139a2bb4fb1a2861ae860f9b0fd98
SKS94 <33518803+SKS94@users.noreply.github.com>
2023-03-16 15:02:25 +0100

    Added final columns to tables (#69)
    * updating columns
    
    * all columns added, and string size streamlined
    
    * all columns added, and string size streamlined

--- src/FINALES2/db/base_class.py
+++ src/FINALES2/db/base_class.py
@@ -13,6 +13,22 @@ class Base:
     id: Any
     __name__: str
 
+    # Small string requirements
+    TENANT_NAME_STRING_SIZE = 100
+    METHOD_STRING_SIZE = 100
+    QUANTITY_STRING_SIZE = 100
+    COST_STRING_SIZE = 100
+    BUDGET_STRING_SIZE = COST_STRING_SIZE
+    STATUS_STRING_SIZE = 100
+    CONTACT_PERSON_STRING_SIZE = 100
+
+    # Large string requirements
+    CAPABILITIES_STRING_SIZE = 5000
+    LIMITATIONS_STRING_SIZE = 5000
+    SPECIFICATIONS_STRING_SIZE = 5000
+    PARAMETERS_STRING_SIZE = 5000
+    DATA_STRING_SIZE = 5000
+
     # to generate tablename from classname
     @declared_attr
     def __tablename__(cls) -> str:
