3d6e872c71d62f566b00ee7f46faf71eb0b2bd47
Francisco Ramirez <ramirezfranciscof@users.noreply.github.com>
2023-03-16 15:36:05 +0000

    Move populate to a different group for dev/test

--- src/FINALES2/cli/database.py
+++ src/FINALES2/cli/database.py
@@ -1,11 +1,7 @@
-import json
-import uuid
-from datetime import datetime
-
 import click
 
-from FINALES2.db import Base, Quantity
-from FINALES2.db.session import engine, get_db
+from FINALES2.db import Base
+from FINALES2.db.session import engine
 
 
 @click.group("db")
@@ -23,38 +19,3 @@ def db_init():
 
     for table in Base.metadata.sorted_tables:
         click.echo(f"   {table.name}")
-
-
-@cli_db.command("populate")
-def db_populate():
-    "Populates the database with initial data for testing"
-
-    dummy_schema = {
-        "type": "object",
-        "properties": {
-            "temperature": {
-                "type": "number",
-                "description": (
-                    "the temperature of the system"
-                    " (this is just a temporary stand in)"
-                ),
-            }
-        },
-        "required": ["temperature"],
-    }
-
-    new_capability = Quantity(
-        **{
-            "uuid": str(uuid.uuid4()),
-            "quantity": "DummyQuantity",
-            "method": "DummyMethod",
-            "specifications": json.dumps(dummy_schema),
-            "is_active": True,
-            "load_time": datetime.now(),
-        }
-    )
-
-    with get_db() as session:
-        session.add(new_capability)
-        session.commit()
-        session.refresh(new_capability)
