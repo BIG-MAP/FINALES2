c41905383cfda1cc7c5ca053a30ac8b733d037e0
Francisco Ramirez <ramirezfranciscof@users.noreply.github.com>
2023-03-17 09:06:21 +0000

    Add cli command to create quantity db entries

--- src/FINALES2/cli/database.py
+++ src/FINALES2/cli/database.py
@@ -1,7 +1,10 @@
+import json
+import uuid
+
 import click
 
-from FINALES2.db import Base
-from FINALES2.db.session import engine
+from FINALES2.db import Base, Quantity
+from FINALES2.db.session import engine, get_db
 
 
 @click.group("db")
@@ -19,3 +22,33 @@ def db_init():
 
     for table in Base.metadata.sorted_tables:
         click.echo(f"   {table.name}")
+
+
+@cli_db.group("add")
+def cli_add():
+    """Commands to add data to the database."""
+
+
+@click.option(
+    "--input-filepath",
+    required=True,
+    prompt="Please provide the path to a json file with the specs",
+    type=str,
+    help="Path to a json file with the specs.",
+)
+@cli_add.command("capability")
+def db_add_capability(input_filepath):
+    "Add an entry to the quantity table."
+
+    with open(input_filepath) as fileobj:
+        capability_data = json.load(fileobj)
+
+    # This should maybe be done through the engine or another internal submodule
+    capability_data["uuid"] = str(uuid.uuid4())
+    capability_data["specifications"] = json.dumps(capability_data["specifications"])
+    new_capability = Quantity(**capability_data)
+
+    with get_db() as session:
+        session.add(new_capability)
+        session.commit()
+        session.refresh(new_capability)
