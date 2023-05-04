665533666222081a2543e7fe6a778e5e6716173d
SKS94 <simonsteensen94@hotmail.com>
2023-05-04 13:33:38 +0000

    remove iskip

--- src/FINALES2/db/__init__.py
+++ src/FINALES2/db/__init__.py
@@ -1,13 +1,10 @@
 from .base_class import Base
+from .tables.link_quantity_request import LinkQuantityRequest
+from .tables.link_quantity_result import LinkQuantityResult
 from .tables.quantities import Quantity
 from .tables.requests import Request
 from .tables.results import Result
 from .tables.tenants import Tenant
-
-# The precommit has been disabled for this file, since the LinkQuantityRequest line
-# is moved above the Quantity line, which breaks the database initialization.
-from .tables.link_quantity_request import LinkQuantityRequest  # isort: skip
-from .tables.link_quantity_result import LinkQuantityResult  # isort: skip
 
 __all__ = [
     "Base",
