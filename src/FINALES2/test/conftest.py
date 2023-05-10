2241ad6fa93c75f43292dfd85e3f0f2152259aa7
MonikaVo <monika.vogler@kit.edu>
2023-05-10 09:54:49 +0000

    Trial to fix errors in tests

--- src/FINALES2/test/conftest.py
+++ src/FINALES2/test/conftest.py
@@ -3,39 +3,37 @@ be used for deployment of the code."""
 
 from pathlib import Path
 
-script_path = Path(__file__).parent.resolve()
 
-host = "localhost"
-port = 13371
+def pytest_configure(config):
+    script_path = Path(__file__).parent.resolve()
 
-user_db: str = str(Path(script_path).joinpath("session_testing.db"))
+    user_db: str = str(Path(script_path).joinpath("session_testing.db"))
 
-secret_key = "for_test_only"
-algorithm = "HS256"
-token_expiration_min = 2
+    secret_key = "for_test_only"
+    algorithm = "HS256"
+    token_expiration_min = 2
 
+    # The following code generates a test config file for the user management, which
+    # will then be used for the test. It is a copy of this configuration file as the
+    # methods enabling the authentication process use the configuration file in the
+    # config_path as a standard, which cannot be changed as a parameter to avoid the
+    # filepath of the user database being exposed in the docs
+    config_base_path = Path(script_path).parents[2]
+    print(config_base_path)
 
-# The following code generates a test config file for the user management, which will
-# then be used for the test. It is a copy of this configuration file as the methods
-# enabling the authentication process use the configuration file in the config_path as
-# a standard, which cannot be changed as a parameter to avoid the filepath of the
-# user database being exposed in the docs
-config_base_path = Path(script_path).parents[2]
-print(config_base_path)
+    config_path = Path(config_base_path).joinpath("files_user_management")
+    if not config_path.is_dir():
+        config_path.mkdir()
+        with open(config_path.joinpath("config_user_manager.py"), "w") as config_file:
+            config_file.write(
+                f"""
+    script_path = {script_path}
 
-config_path = Path(config_base_path).joinpath("files_user_management")
-if not config_path.is_dir():
-    config_path.mkdir()
-    with open(config_path.joinpath("config_user_manager.py"), "w") as config_file:
-        config_file.write(
-            f"""
-script_path = {script_path}
+    user_db: str = {user_db}
 
-user_db: str = {user_db}
-
-secret_key = {secret_key}
-algorithm = {algorithm}
-token_expiration_min = {token_expiration_min}
-        """
-        )
-print("File created.")
+    secret_key = {secret_key}
+    algorithm = {algorithm}
+    token_expiration_min = {token_expiration_min}
+            """
+            )
+    print("File created.")
