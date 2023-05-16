6c72384b26fd7421d3c3db4b00789e2a9584f4c5
MonikaVo <monika.vogler@kit.edu>
2023-05-16 06:13:04 +0000

    Comment on dummy test method added

--- src/FINALES2/test/test_dummy.py
+++ src/FINALES2/test/test_dummy.py
@@ -1,2 +1,5 @@
 def test_dummy():
+    """This method only exists for the GitHub action to work as this fails, if
+    no tests are found. It must be removed once the actual tests are re-activated.
+    """
     assert 1 == 1
